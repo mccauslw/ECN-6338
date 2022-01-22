aaeff198e0ec0b18f5abc5dac6dbf55e699d603d
William McCausland <mccauslw@mac.com>
2022-01-22 15:53:55 -0500

    Add b using Chol, edit exercise

--- GPA.R
+++ GPA.R
@@ -17,10 +17,20 @@ GPA_res <- lm(colGPA ~ hsGPA + ACT, data=gpa1)
 summary(GPA_res)
 
 # Construire le vecteur y et la matrice X
-y = gpa1$colGPA
-n = length(y)
-X = cbind(rep(1, n), gpa1$hsGPA, gpa1$ACT)
+y <- gpa1$colGPA
+n <- length(y)
+X <- cbind(rep(1, n), gpa1$hsGPA, gpa1$ACT)
 
 # Calculer l'estimation MCO b avec la fonction solve, qui utilise
 # la décomposition LU
-b = solve(t(X) %*% X, t(X) %*% y)
+XX <- t(X) %*% X
+Xy <- t(X) %*% y
+b_LU <- solve(XX, Xy)        # MCO b, avec LU
+
+# Calculer l'estimation MCO b avec la décomposition de Cholesky
+# Les fonctions pertinentes sont chol et backsolve
+R <- chol(XX)
+Rb <- forwardsolve(R, Xy, upper.tri=T, transpose=T)
+b_Chol <- backsolve(R, Rb)   # MCO b, avec Cholesky
+
+# Une exercice est de calcular l'estimation MCO b avec la décomposition QR
